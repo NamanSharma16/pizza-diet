9aa22c8eceff17ced445fb11f5be384a723219a6
namansharma16 <namansharma1234@gmail.com>
2023-05-14 18:13:27 +0530

    changes in files

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,7 @@ const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 window.addEventListener("scroll", function () {
   if (window.scrollY >= 100) {
-    header.classList.add("active");
+    // header.classList.add("active");
     backTopBtn.classList.add("active");
   } else {
     header.classList.remove("active");
